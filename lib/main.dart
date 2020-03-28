902ca855dcf5c2bef49c2f89f9b3cec82ed7b701
Reverope <shatish123456789@gmail.com>
2020-03-28 20:23:25 +0530

    Splash Screen Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 //import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:veli/payment.dart';
+
 ///import 'package:shared_preferences/shared_preferences.dart';
 import 'login_page.dart';
 import 'package:splashscreen/splashscreen.dart';
@@ -21,10 +22,26 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return new SplashScreen(
-        seconds: 2,
-        image: Image.asset('asset/vl4.png'),
-        photoSize: 60.0,
-        navigateAfterSeconds: LoginPage());
+      seconds: 4,
+      navigateAfterSeconds: new LoginPage(),
+      title: new Text(
+        'Veli',
+        style: new TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 40.0,
+            fontFamily: "sam",
+            color: Colors.black87),
+      ),
+      backgroundColor: Colors.white,
+      loadingText: Text(
+        "made with love for",
+        style: TextStyle(fontFamily: "QuickSand", color: Colors.black38),
+      ),
+      subtitle: Text(
+        "NIT Silchar",
+        style: TextStyle(fontFamily: "QuickSand", color: Colors.black38),
+      ),
+      photoSize: 100.0,
+    );
   }
 }
-
